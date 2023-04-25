d849a581d2d28db4b0c1b9be3837f362bf93fabf
Pavan Kalyan Thota <pavanyadav.thota@gmail.com>
2023-04-24 20:03:30 -0400

    just post

--- server.py
+++ server.py
@@ -36,36 +36,11 @@ class DataResource(resource.Resource):
 
         return aiocoap.Message(code=aiocoap.CREATED, payload=request.payload)
 
-class BlockResource(resource.Resource):
-    """Example resource which supports the GET and PUT methods. It sends large
-    responses, which trigger blockwise transfer."""
-    def __init__(self):
-        super().__init__()
-        self.set_content(b"This is the resource's default content. It is padded "
-                b"with numbers to be large enough to trigger blockwise "
-                b"transfer.\n")
-
-    def set_content(self, content):
-        self.content = content
-        while len(self.content) <= 1024:
-            self.content = self.content + b"0123456789\n"
-
-    async def render_get(self, request):
-        return aiocoap.Message(payload=self.content)
-
-    async def render_put(self, request):
-        print('PUT payload: %s' % request.payload)
-        self.set_content(request.payload)
-        return aiocoap.Message(code=aiocoap.CHANGED, payload=self.content)
-
-
-
 async def main():
     root = resource.Site()
 
     root.add_resource(['time'], TimeResource())
     root.add_resource(['data'], DataResource())
-    root.add_resource(['block'], BlockResource())
 
     await aiocoap.Context.create_server_context(root, bind= ["localhost", 8000])
 
